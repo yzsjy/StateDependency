2053338ebc6ca615088c08db631f2288d020738d
yzsjy <sunjunyan>
2021-07-14 20:56:14 +0800

    add function to acquire dependency path

--- src/main/java/neu/lab/dependency/vo/ArtifactResolver.java
+++ src/main/java/neu/lab/dependency/vo/ArtifactResolver.java
@@ -1,37 +1,89 @@
 package neu.lab.dependency.vo;
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DefaultArtifact;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.factory.DefaultArtifactFactory;
-import org.apache.maven.artifact.handler.manager.DefaultArtifactHandlerManager;
-import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
-import org.apache.maven.artifact.versioning.VersionRange;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author SUNJUNYAN
  */
 public class ArtifactResolver {
 
-    public void resolver() throws InvalidVersionSpecificationException {
-        Artifact artifact = getArtifact("com.google.guava", "guava", "30.1.1-jre", "jar", "", "compile");
-        Artifact newArtifact = new DefaultArtifact("com.google.guava", "guava", VersionRange.createFromVersionSpec("30.1.1-jre"), "compile", "jar", "", new DefaultArtifactHandlerManager().getArtifactHandler("compile"));
-        System.out.println(artifact.getDownloadUrl());
+    private static ArtifactResolver instance;
+
+    private ArtifactResolver() {
+
     }
 
-    public Artifact getArtifact(String groupId, String artifactId, String versionRange, String type, String classifier,
-                                String scope) {
-        ArtifactFactory factory = new DefaultArtifactFactory();
-        try {
-            return factory.createDependencyArtifact(groupId, artifactId,
-                    VersionRange.createFromVersionSpec(versionRange), type, classifier, scope);
-        } catch (InvalidVersionSpecificationException e) {
-            System.err.println("cant create Artifact!" + e);
-            return null;
+    public static ArtifactResolver i() {
+        if (instance == null) {
+            instance = new ArtifactResolver();
         }
+        return instance;
     }
 
-    public static void main(String[] args) throws InvalidVersionSpecificationException {
-        new ArtifactResolver().resolver();
+    public String resolver(String groupId, String artifactId, String version) {
+        boolean canResolve = mvnGetArtifact(groupId, artifactId, version);
+        String path = null;
+        if (canResolve) {
+            String repository = mvnGetRepository();
+            path = repository
+                    + File.separator + groupId.replace(".", File.separator) +File.separator
+                    + artifactId.replace(".", File.separator) + File.separator
+                    + version + File.separator + artifactId + "-" + version + ".jar";
+        }
+        return path;
+    }
+
+    public boolean mvnGetArtifact(String groupId, String artifactId, String version) {
+        System.out.println("Start to get target dependency...");
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
+        CommandLine cmdLine = CommandLine.parse("cmd /k mvn dependency:get -DgroupId=" + groupId + " -DartifactId=" + artifactId + " -Dversion=" + version);
+        DefaultExecutor executor = new DefaultExecutor();
+        try {
+            executor.setStreamHandler(streamHandler);
+            executor.execute(cmdLine);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        boolean bulidSuccess = false;
+        for (String line : outputStream.toString().split("\\n")) {
+            if (line.contains("BUILD SUCCESS")) {
+                bulidSuccess = true;
+                System.out.println("Successfully get target dependency...");
+            }
+        }
+        return bulidSuccess;
+    }
+
+    public String mvnGetRepository() {
+        System.out.println("Start to get repository path...");
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
+        CommandLine cmdLine = CommandLine.parse("cmd /k mvn help:effective-settings");
+        DefaultExecutor executor = new DefaultExecutor();
+        try {
+            executor.setStreamHandler(streamHandler);
+            executor.execute(cmdLine);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String repository = "D:\\Maven\\Repositories";
+        for (String line : outputStream.toString().split("\\n")) {
+            if (line.contains("<localRepository>")) {
+                repository = line.split("<localRepository>")[1].split("</localRepository>")[0];
+                System.out.println("Successfully get repository path...");
+            }
+        }
+        return repository;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ArtifactResolver().resolver("com.google.guava", "guava", "21.0"));
     }
 }
